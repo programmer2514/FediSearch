9b10bdc1b2096fff10e1b0e98aa9d2b0f97f7334
programmer2514 <43104632+programmer2514@users.noreply.github.com>
2023-06-19 01:16:52 -0400

    Major update
    * New query algorithm
    * Cleaned up formatting
    * Added donation links
    * Improved screen reader compatibility

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-let preferred_engine = localStorage.getItem("preferredEngine");
+var preferred_engine = localStorage.getItem("preferredEngine");
 
 if (!preferred_engine) {
-    preferred_engine = "ddg";
+    preferred_engine = "google_acc";
     localStorage.setItem("preferredEngine", preferred_engine);
 }
 
 document.getElementById("engine").value = preferred_engine;
 
+var network_toggles = localStorage.getItem("networkToggles").split(',').map(x => (x == 'true') ? true : false);
+
+if (!network_toggles) {
+    network_toggles = [true, true, false, false];
+    localStorage.setItem("networkToggles", network_toggles);
+}
+
+document.getElementById("lemmy-toggle").checked = network_toggles[0];
+document.getElementById("kbin-toggle").checked = network_toggles[1];
+document.getElementById("mastodon-toggle").checked = network_toggles[2];
+document.getElementById("peertube-toggle").checked = network_toggles[3];
+
+document.getElementById("search").value = sessionStorage.getItem("searchQuery");
+
+updateEngine();
 handleTyping();
+
 document.getElementById("search").focus();
 
 
 
 function searchRedirect(e) {
 
-    let preferred_engine = document.getElementById("engine").value;
-    let search_query = document.getElementById("search").value;
+    var preferred_engine = document.getElementById("engine").value;
+    var search_query = document.getElementById("search").value;
 
-    let search_engine = "https://duckduckgo.com/?q=";
+    var search_engine = "https://duckduckgo.com/?q=";
 
     if (preferred_engine === "google")
         search_engine = "https://www.google.com/search?q=";
@@ -28,22 +44,76 @@ function searchRedirect(e) {
     else if (preferred_engine === "dogpile")
         search_engine = "https://www.dogpile.com/serp?q=";
 
-    if (search_query)
-        window.location.href = `${search_engine}${search_query}+site%3Akbin.social+OR+site%3Alemmy.ml+OR+site%3Afedia.io+OR+site%3Apawb.social+OR+site%3Abeehaw.org+OR+site%3Alemmy.blahaj.zone+OR+site%3Alemmy.dbzer0.com+OR+site%3Alemmy.world+OR+site%3Ahexbear.net+OR+site%3Alemmy.one+OR+site%3Afeddit.de+OR+site%3Alemmy.fmhy.ml+OR+site%3Ash.itjust.works+OR+site%3Alemmynsfw.com+OR+site%3Aprogramming.dev`;
+    if (search_query) {
+        if (preferred_engine !== "google_acc")
+            window.location.href = `${search_engine}${search_query}+site%3Akbin.social+OR+site%3Alemmy.ml+OR+site%3Afedia.io+OR+site%3Apawb.social+OR+site%3Abeehaw.org+OR+site%3Alemmy.blahaj.zone+OR+site%3Alemmy.dbzer0.com+OR+site%3Alemmy.world+OR+site%3Ahexbear.net+OR+site%3Alemmy.one+OR+site%3Afeddit.de+OR+site%3Alemmy.fmhy.ml+OR+site%3Ash.itjust.works+OR+site%3Alemmynsfw.com+OR+site%3Aprogramming.dev`;
+        else {
+            var network_toggles = [document.getElementById("lemmy-toggle").checked,
+            document.getElementById("kbin-toggle").checked,
+            document.getElementById("mastodon-toggle").checked,
+            document.getElementById("peertube-toggle").checked];
+
+            var lemmy_query = 'intext:"modlog" "instances" "docs" "code" "join lemmy"',
+                kbin_query = 'intext:"powered by kbin"',
+                mastodon_query = 'intext:"part of the decentralized social network powered by mastodon"',
+                peertube_query = 'intext:"powered by peertube"';
+
+            // I'm too tired to write this correctly
+            // I sincerely apologize to anyone who has to figure this out
+            var nlist_query = (network_toggles[0] ? lemmy_query : "") +
+                ((network_toggles[0] && (network_toggles[1] || network_toggles[2] || network_toggles[3])) ? " OR " : "") +
+                (network_toggles[1] ? kbin_query : "") +
+                ((network_toggles[1] && (network_toggles[2] || network_toggles[3])) ? " OR " : "") +
+                (network_toggles[2] ? mastodon_query : "") +
+                ((network_toggles[2] && network_toggles[3]) ? " OR " : "") +
+                (network_toggles[3] ? peertube_query : "");
+
+            window.location.href = `https://www.google.com/search?q=${search_query}+${nlist_query}`;
+        }
+        sessionStorage.setItem("searchQuery", search_query);
+    }
+
     e.preventDefault();
     return false;
 }
 
 function updateEngine() {
 
-    let preferred_engine = document.getElementById("engine").value;
+    var network_toggles = [document.getElementById("lemmy-toggle").checked,
+    document.getElementById("kbin-toggle").checked,
+    document.getElementById("mastodon-toggle").checked,
+    document.getElementById("peertube-toggle").checked];
+
+    localStorage.setItem("networkToggles", network_toggles);
+
+    var preferred_engine = document.getElementById("engine").value;
+
+    if (preferred_engine === "google_acc") {
+        document.getElementsByTagName("h3")[0].style.display = "initial";
+        document.getElementById("network-selector").style.display = "grid";
+        document.querySelectorAll('[type="checkbox"]').forEach(checkbox => {
+            checkbox.style.display = "initial";
+        });
+        document.querySelectorAll(".checkbox-label").forEach((label) => {
+            label.style.display = "initial";
+        });
+    } else {
+        document.getElementsByTagName("h3")[0].style.removeProperty("display");
+        document.getElementById("network-selector").style.removeProperty("display");
+        document.querySelectorAll('[type="checkbox"]').forEach(checkbox => {
+            checkbox.style.removeProperty("display");
+        });
+        document.querySelectorAll(".checkbox-label").forEach(label => {
+            label.style.removeProperty("display");
+        });
+    }
 
     localStorage.setItem("preferredEngine", preferred_engine);
     return false;
 }
 
 function handleTyping() {
-    let search_query = document.getElementById("search").value;
+    var search_query = document.getElementById("search").value;
 
     if (search_query) {
         document.getElementById("submit").style.background = "rgb(33, 150, 243)";
