2f0b87a7e04207de5fb437bd64f58b9f28ab1aba
Glory <72595589+eat-your-glory@users.noreply.github.com>
2025-03-13 19:46:05 +0100

    Added Kagi Search Engine

--- script.js
+++ script.js
@@ -39,6 +39,8 @@ function searchRedirect(e) {
         search_engine = "https://search.yahoo.com/search?p=";
     else if (preferred_engine === "dogpile")
         search_engine = "https://www.dogpile.com/serp?q=";
+    else if (preferred_engine === "kagi")
+        search_engine = "https://kagi.com/search?q="
 
     if (search_query) {
         if (preferred_engine.includes("_acc")) {
